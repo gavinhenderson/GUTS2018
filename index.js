0ac8487e376f8ebc7348bcfcaea012fdbb068ded
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2018-10-12 22:26:36 +0100

    Added a public folder and a basic css

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const io = require("socket.io")(http);
 // Config
 const port = process.env.PORT || 8080;
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 // Root
 app.get("/", (req, res) => {
