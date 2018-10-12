f4750c53257373ce470017480e52a5833c8f2266
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2018-10-12 22:05:28 +0100

    Added basic ejs route

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 // Dependencies
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 
 // Config
 const port = process.env.PORT || 8080;
+app.set("view engine", "ejs");
 
 // Root
-app.get('/', (req, res) => {
-    res.send('Hello world');
+app.get("/", (req, res) => {
+  res.render("home", { world: "world" });
 });
 
-io.on('connection', (socket) => {
-    console.log('Socket connected: ' + socket.id);
+io.on("connection", (socket) => {
+  console.log("Socket connected: " + socket.id);
 });
 
 http.listen(port, () => {
-    console.log('Listening on port ' + port);
+  console.log("Listening on port " + port);
 });
