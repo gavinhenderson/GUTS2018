8aded48c4d337b28396a5657cb713c2d446609c5
Adam Hirst <adam@ahirst.com>
2018-10-12 21:31:01 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+import { isBoolean } from 'util';
+// Dependencies
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+// Config
+const port = process.env.PORT || 8080;
+
+// Root
+app.get('/', (req, res) => {
+    res.send('Hello world');
+});
+
+io.on('connection', (socket) => {
+    console.log('Socket connected: ' + socket.id);
+});
+
+http.listen(port, () => {
+    console.log('Listening on port ' + port);
+});
