dc440caed77beb7f02c17c77f667f611ad7156a9
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2018-10-13 00:15:20 +0100

    Basic auth setup

--- index.js
+++ index.js
@@ -3,16 +3,47 @@ const express = require("express");
 const app = express();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const routes = require("./app/routes.js");
+const secret = require("./users.js");
 
 // Config
 const port = process.env.PORT || 8080;
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-// Root
-app.get("/", (req, res) => {
-  res.render("home", { world: "world" });
+// passport
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+
+passport.use(
+  new LocalStrategy(function(username, password, done) {
+    if (username === "admin" && password === "admin") {
+      done(null, { user: username });
+    } else {
+      done(null, false);
+    }
+  }),
+);
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
 });
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
+app.use(session({ secret: "conorhatestrains" }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+routes(app);
 
 io.on("connection", (socket) => {
   console.log("Socket connected: " + socket.id);
---
+++ users.js
@@ -0,0 +1,26 @@
+const uuidv4 = require("uuid/v4");
+
+const users = [
+  {
+    number: "1",
+    name: "Mitchell McJannett",
+  },
+  {
+    name: "Gavin Henderson",
+    number: "2",
+  },
+  {
+    name: "Jay Bennett",
+    number: "3",
+  },
+  {
+    name: "Dean Beggs",
+    number: "4",
+  },
+];
+
+users.forEach((user) => {
+  user.id = uuidv4();
+});
+
+module.exports = { users };
