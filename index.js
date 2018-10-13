8ed0ff10ccea5d56f2bb5fb09295afbc18d1d0b5
Conor Haining <conor.haining@gmail.com>
2018-10-13 16:54:20 +0100

    Cross origin allowing

--- index.js
+++ index.js
@@ -44,6 +44,12 @@ app.use(session({ secret: "conorhatestrains" }));
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 routes(app, passport);
 
 io.on("connection", (socket) => {
