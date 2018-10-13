9bb1f32318423f42fff98d1b079d591cba1f0211
Conor Haining <conor.haining@gmail.com>
2018-10-13 19:39:57 +0100

    Tokens for each handle

--- users.js
+++ users.js
@@ -4,21 +4,25 @@ const users = [{
     number: "1",
     name: "Mitchell McJannett",
     title: "Forward Operator",
+    clientName: "MitchellMcJannett",
   },
   {
     name: "Gavin Henderson",
     number: "2",
     title: "Backward Operator",
+    clientName: "GavinHenderson",
   },
   {
     name: "Jay Bennett",
     number: "3",
     title: "Head Operator",
+    clientName: "JayBennett",
   },
   {
     name: "Dean Beggs",
     number: "4",
     title: "The Operator",
+    clientName: "DeanBeggs",
   },
 ];
 
