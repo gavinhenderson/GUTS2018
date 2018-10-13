82b5c4d5309924b2e37edac438639c45f4f9af12
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2018-10-13 21:22:23 +0100

    Moved to another class instead

--- public/js/timer.js
+++ public/js/timer.js
@@ -1,23 +1,30 @@
 class Timer {
-    constructor(timer) {
-        this.timer = document.querySelector('.' + timer);
+  constructor(timer) {
+    this.timer = document.querySelector("." + timer);
 
-        this.counter = 0;
-    }
+    this.counter = 0;
+  }
 
-    start() {
-        this.interval = setInterval(() => {
-            const minutes = Math.floor(this.counter / 60);
-            const seconds = this.counter % 60;
-            const timeGoing = `${String(minutes).padStart(2,0)}:${String(seconds).padStart(2,0)}`;
+  start() {
+    this.interval = setInterval(() => {
+      const minutes = Math.floor(this.counter / 60);
+      const seconds = this.counter % 60;
+      const timeGoing = `${String(minutes).padStart(2, 0)}:${String(
+        seconds,
+      ).padStart(2, 0)}`;
 
-            this.timer.innerHTML = timeGoing
+      this.timer.innerHTML = timeGoing;
 
-            this.counter++;
-        }, 1000);
-    }
+      this.counter++;
+    }, 1000);
+  }
 
-    stop() {
-        clearInvterval(this.interval)
-    }
-};
+  stop() {
+    clearInterval(this.interval);
+  }
+
+  reset() {
+    this.counter = 0;
+    this.timer.innerHTML = "00:00";
+  }
+}
