240f93f83ff7c2056c640410ff74f05602062269
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2018-10-13 20:20:18 +0100

    admin route now added

--- users.js
+++ users.js
@@ -1,28 +1,33 @@
 const uuidv4 = require("uuid/v4");
 
-const users = [{
+const users = [
+  {
     number: "1",
     name: "Mitchell McJannett",
     title: "Forward Operator",
     clientName: "MitchellMcJannett",
+    admin: false,
   },
   {
     name: "Gavin Henderson",
     number: "2",
     title: "Backward Operator",
     clientName: "GavinHenderson",
+    admin: true,
   },
   {
     name: "Jay Bennett",
     number: "3",
     title: "Head Operator",
     clientName: "JayBennett",
+    admin: false,
   },
   {
     name: "Dean Beggs",
     number: "4",
     title: "The Operator",
     clientName: "DeanBeggs",
+    admin: false,
   },
 ];
 
