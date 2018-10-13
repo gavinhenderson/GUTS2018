53033a6b4b23381324aefba1b964949e28d8fda9
mmcjannett <mitchell.mcjannett@btinternet.com>
2018-10-13 12:51:30 +0100

    Added in different roles.

--- users.js
+++ users.js
@@ -4,18 +4,22 @@ const users = [
   {
     number: "07597347312",
     name: "Mitchell McJannett",
+    title: "Forward Operator",
   },
   {
     name: "Gavin Henderson",
     number: "07414525394",
+    title: "Backward Operator",
   },
   {
     name: "Jay Bennett",
     number: "07754611783",
+    title: "Middle Operator",
   },
   {
     name: "Dean Beggs",
     number: "07495462969",
+    title: "Operator",
   },
 ];
 
