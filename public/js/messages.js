216bac76e70a911c3d37c51ed1efdeb75d42d617
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2018-10-13 16:10:40 +0100

    message now scroll, only in chrome

--- public/js/messages.js
+++ public/js/messages.js
@@ -1,39 +1,31 @@
-  class MessageList {
-      constructor(listClassName) {
-          this.listElement = document.querySelector("." + listClassName);
-      };
+class MessageList {
+  constructor(listClassName) {
+    this.listElement = document.querySelector("." + listClassName);
+  }
 
-      setOperator(operator) {
-          this.operator = operator;
-      };
+  setOperator(operator) {
+    this.operator = operator;
+  }
 
-      setCaller(caller) {
-          this.caller = caller;
-      };
+  setCaller(caller) {
+    this.caller = caller;
+  }
 
-      addMessage(personIdentifier, message) {
-          const person = personIdentifier === "caller" ? this.caller : this.operator;
-          const newMessage = document.createElement("div");
-          newMessage.classList.add(`${personIdentifier}-message-container`)
-          newMessage.innerHTML = `
-            <p class="${personIdentifier}-message-name"><strong>${person.name}</strong> (${personIdentifier === "caller" ? "Customer" :"You"})</p>
+  addMessage(personIdentifier, message) {
+    const person = personIdentifier === "caller" ? this.caller : this.operator;
+    const newMessage = document.createElement("div");
+    newMessage.classList.add(`${personIdentifier}-message-container`);
+    newMessage.innerHTML = `
+            <p class="${personIdentifier}-message-name"><strong>${
+      person.name
+    }</strong> (${personIdentifier === "caller" ? "Customer" : "You"})</p>
             <div class="${personIdentifier}-message-content">
                 <p class="message-text">${message}</p>
             </div>
           `;
-          this.listElement.appendChild(newMessage);
-          const parent = this.listElement.parentElement;
-          const target = parent.scrollTop + newMessage.clientHeight;
-          smoothScroll(parent, target);
-      };
-  }
+    this.listElement.appendChild(newMessage);
 
-  function smoothScroll(element, targetValue) {
-      const interval = setInterval(() => {
-          if (element.scrollTop >= targetValue) {
-              clearInterval(interval);
-              return;
-          }
-          element.scrollTop += 1;
-      }, 1);
+    const parent = this.listElement.parentElement;
+    parent.scrollTop += newMessage.offsetHeight;
   }
+}
