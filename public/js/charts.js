b397c65fc481c5dec6944fcdaf9c58fd130f93b1
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2018-10-14 10:17:44 +0100

    Emotion char t now uses real data

--- public/js/charts.js
+++ public/js/charts.js
@@ -3,11 +3,9 @@ window.onload = function() {
     const lineChartData = data.sentiment.map((current) => {
       return {
         y: current.sentimentScore,
-        x: new Date(current.timestamp * 1000),
+        x: new Date(current.timestamp),
       };
     });
-
-    console.log(lineChartData);
 
     var chart = new CanvasJS.Chart("chartContainer", {
       animationEnabled: true,
@@ -30,6 +28,60 @@ window.onload = function() {
       ],
     });
     chart.render();
+
+    const titleCase = (lowercase) => {
+      return lowercase[0].toUpperCase() + lowercase.substr(1);
+    };
+
+    const chartLabels = Object.keys(data.emotion).map((current) =>
+      titleCase(current),
+    );
+
+    const chartData = Object.keys(data.emotion).map(
+      (current) => data.emotion[current],
+    );
+
+    var ctx2 = document.getElementById("emotionsChart").getContext("2d");
+    var polarChart = new Chart(ctx2, {
+      type: "radar",
+      data: {
+        labels: chartLabels,
+        datasets: [
+          {
+            label: "Number of calls (minutes, customers, frequency)",
+            data: chartData,
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(255, 206, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+              "rgba(153, 102, 255, 0.2)",
+              "rgba(255, 159, 64, 0.2)",
+            ],
+            borderColor: [
+              "rgba(255,99,132,1)",
+              "rgba(54, 162, 235, 1)",
+              "rgba(255, 206, 86, 1)",
+              "rgba(75, 192, 192, 1)",
+              "rgba(153, 102, 255, 1)",
+              "rgba(255, 159, 64, 1)",
+            ],
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+              },
+            },
+          ],
+        },
+      },
+    });
   });
 
   var ctx = document.getElementById("myChart").getContext("2d");
@@ -72,48 +124,6 @@ window.onload = function() {
               r: 15,
             },
           ],
-          backgroundColor: [
-            "rgba(255, 99, 132, 0.2)",
-            "rgba(54, 162, 235, 0.2)",
-            "rgba(255, 206, 86, 0.2)",
-            "rgba(75, 192, 192, 0.2)",
-            "rgba(153, 102, 255, 0.2)",
-            "rgba(255, 159, 64, 0.2)",
-          ],
-          borderColor: [
-            "rgba(255,99,132,1)",
-            "rgba(54, 162, 235, 1)",
-            "rgba(255, 206, 86, 1)",
-            "rgba(75, 192, 192, 1)",
-            "rgba(153, 102, 255, 1)",
-            "rgba(255, 159, 64, 1)",
-          ],
-          borderWidth: 1,
-        },
-      ],
-    },
-    options: {
-      scales: {
-        yAxes: [
-          {
-            ticks: {
-              beginAtZero: true,
-            },
-          },
-        ],
-      },
-    },
-  });
-
-  var ctx2 = document.getElementById("polarChart").getContext("2d");
-  var polarChart = new Chart(ctx2, {
-    type: "polarArea",
-    data: {
-      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-      datasets: [
-        {
-          label: "Number of calls (minutes, customers, frequency)",
-          data: [10, 20, 30, 5, 12, 17],
           backgroundColor: [
             "rgba(255, 99, 132, 0.2)",
             "rgba(54, 162, 235, 0.2)",
