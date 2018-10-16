51b87a5a195d3a9f9d8c29925ecb15647eb6193e
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2018-10-16 11:59:48 +0100

    Fixed merging in users

--- users.js
+++ users.js
@@ -2,11 +2,7 @@ const uuidv4 = require("uuid/v4");
 
 const users = [
   {
-<<<<<<< HEAD
     number: "1",
-=======
-    number: "1",
->>>>>>> b8d09c395f7f8cf9a45eff428c861369c0698700
     name: "Mitchell McJannett",
     title: "Forward Operator",
     clientName: "MitchellMcJannett",
@@ -14,44 +10,28 @@ const users = [
   },
   {
     name: "Gavin Henderson",
-<<<<<<< HEAD
     number: "2",
-=======
-    number: "2",
->>>>>>> b8d09c395f7f8cf9a45eff428c861369c0698700
     title: "Backward Operator",
     clientName: "GavinHenderson",
     admin: true,
   },
   {
     name: "Jay Bennett",
-<<<<<<< HEAD
     number: "3",
-=======
-    number: "3",
->>>>>>> b8d09c395f7f8cf9a45eff428c861369c0698700
     title: "Head Operator",
     clientName: "JayBennett",
     admin: false,
   },
   {
     name: "Dean Beggs",
-<<<<<<< HEAD
     number: "4",
-=======
-    number: "4",
->>>>>>> b8d09c395f7f8cf9a45eff428c861369c0698700
     title: "The Operator",
     clientName: "DeanBeggs",
     admin: false,
   },
   {
     name: "Conor Haining",
-<<<<<<< HEAD
     number: "5",
-=======
-    number: "5",
->>>>>>> b8d09c395f7f8cf9a45eff428c861369c0698700
     title: "The Big Man",
     clientName: "ConorHaining",
     admin: false,
